b48378fab55ff8edf849b78f1e9d64b2e69ec156
Dmitry Nesterov <dim.nesterov2015@gmail.com>
2025-01-07 12:58:46 +0000

    fix locale path

--- 12_InstallPackaging/build.sh
+++ 12_InstallPackaging/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
 
-autoreconf -fisv && ./configure && make
+autoreconf -fisv && ./configure && make AM_CFLAGS=-D'LOCALE_PATH=\"po\"' all
 make -C po/ update-po
 install -Dm644 po/ru.gmo po/ru/LC_MESSAGES/guess.mo
